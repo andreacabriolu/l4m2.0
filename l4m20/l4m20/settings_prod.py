46c3b654b123a77b2e4f7edfb58e494f29ada17d
Andrea Cabriolu <andrea.cabriolu@inaf.it>
2024-08-19 18:21:26 +0200

    configuring some setting for first deployment

--- l4m20/l4m20/settings_prod.py
+++ l4m20/l4m20/settings_prod.py
@@ -15,17 +15,23 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Support env variables from .env file if defined
+import os
+from dotenv import load_dotenv
+env_path = load_dotenv(os.path.join(BASE_DIR, '.env'))
+load_dotenv(env_path)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x1n!q!$+c7wivg#m95+azjb)lonp-foy6two$dh@x1^&92=!f0'
+# SECRET_KEY = 'django-insecure-x1n!q!$+c7wivg#m95+azjb)lonp-foy6two$dh@x1^&92=!f0'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-x1n!q!$+c7wivg#m95+azjb)lonp-foy6two$dh@x1^&92=!f0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-import os
+# DEBUG = False
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-# DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -46,7 +52,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -95,14 +100,6 @@ DATABASES = {
     }
 }
 
-# Update database configuration from $DATABASE_URL environment variable (if defined)
-import dj_database_url
-
-if 'DATABASE_URL' in os.environ:
-    DATABASES['default'] = dj_database_url.config(
-        conn_max_age=500,
-        conn_health_checks=True,
-    )
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -139,18 +136,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfilesBASE_DIR'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Static file serving.
-# https://whitenoise.readthedocs.io/en/stable/django.html#add-compression-and-caching-support
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
