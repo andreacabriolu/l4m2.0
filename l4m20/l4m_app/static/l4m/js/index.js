215b68acca88feb22d425ce8552351eb92b2e9ec
Andrea Cabriolu <andrea.cabriolu@inaf.it>
2024-08-31 03:41:46 +0200

    added correct filter and close button

--- l4m20/l4m_app/static/l4m/js/index.js
+++ l4m20/l4m_app/static/l4m/js/index.js
@@ -1,4 +1,10 @@
+let dlg;
+let search;
+
 window.addEventListener('DOMContentLoaded', event => {
+
+    dlg = document.getElementById('dlg open');
+    search = document.getElementById('modal-ob-search');
 
     gkdivs = document.getElementsByClassName('gk-div');
     gkcont = document.getElementById('gks');
@@ -6,37 +12,35 @@ window.addEventListener('DOMContentLoaded', event => {
     gkcont.addEventListener("click", function (e) {
         openDialog(e['id']);
     })
+
+    var closeM = document.getElementsByClassName("close")[0];
+    
+    closeM.onclick = function() {
+        dlg.close();
+        search.value='';
+  }
 })
 
-var modal = document.getElementById('modal-ob-search');
-
-
 function openDialog(e) {
-    dlg = document.getElementById('dlg open');
     dlg.showModal();
 }
 
-var closeM = document.getElementsByClassName("close")[0];
-closeM.onclick = function() {
-    modal.style.display = "none";
-  }
-
 function searchPlayer() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('modal-ob-search');
-    filter = input.value.toUpperCase();
+    var filter, i, txtValue;
+    filter = search.value.toUpperCase();
     dl = document.getElementById("dataList");
     dt = dl.getElementsByTagName('dt');
 
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < dt.length; i++) {
-        a = dt[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
+        txtValue = dt[i].textContent;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
             dt[i].style.display = "";
         } else {
             dt[i].style.display = "none";
         }
     }
+
+    // $('#modal-pl-id').val = e;
 }
 
