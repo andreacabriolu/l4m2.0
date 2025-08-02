9b2999740a5a27e2a8721b056f521115fb03f5ce
Andrea Cabriolu <andrea.cabriolu@inaf.it>
2025-08-03 01:28:03 +0200

    fixing max and min and adding last features

--- l4m20/l4m_app/static/l4m/js/auction.js
+++ l4m20/l4m_app/static/l4m/js/auction.js
@@ -137,6 +137,22 @@ function closeDlg(el) {
 
 }
 
+function setPlayerDialog(player, mode='std') {
+    if(mode == 'high') {
+        $('#dlg_player_open').removeClass('dlg-player');
+        $('#dlg_player_open').addClass('dlg-player-high');
+        $('#notafford').attr('hidden', false);
+        $('#modal-pl-betamount').val(parseInt(player.betamount));   
+        $('#btnSendBet').attr('disabled', true);
+    }
+    else {
+        $('#dlg_player_open').removeClass('dlg-player-high');
+        $('#dlg_player_open').addClass('dlg-player');
+        $('#notafford').attr('hidden', true);
+        $('#btnSendBet').attr('disabled', false);
+    }
+}
+
 function openPlayerDialog(player) {
 
     if (!Object.is(player.name, undefined)) {
@@ -149,16 +165,36 @@ function openPlayerDialog(player) {
     $('#modal-pl-name').val(player.name + player.surname);
     $('#modal-pl-realteam').val(player.realteam);
     $('#modal-pl-role').val(RoleNames[player.role]);
+
+    var balance_for_bets = $('#balance_for_bets').val();
+
     if (player.betamount != 'None') {
-        $('#modal-pl-betamount').val(parseInt(player.betamount) + 1);
-        $('#modal-pl-betamount').attr({"min" : parseInt(player.betamount) + 1}); 
+        if(parseInt(player.betamount) >= parseInt(balance_for_bets)) { //UNAFFORDABLE
+            setPlayerDialog(player, 'high');
+        }
+        else {
+            setPlayerDialog(player);
+            $('#modal-pl-betamount').val(parseInt(player.betamount) + 1);   
+            $('#modal-pl-betamount').attr({"min" : parseInt(player.betamount) + 1});
+        }
     }
     else {
-        $('#modal-pl-betamount').val('');
-        $('#modal-pl-betamount').attr({"min" : 1}); 
+        if(parseInt(balance_for_bets) <= 0) {
+            setPlayerDialog(player, 'high');
+        }
+        else {
+            setPlayerDialog(player);
+            $('#modal-pl-betamount').val('');
+            $('#modal-pl-betamount').attr({"min" : 1});
+        } 
     }
 
-    $('#modal-currentbet').hide(); //TODO: check here for player displaying
+    $('#modal-label-bet').html($('#modal-label-bet').html()
+        .replace('_minbet_', player.betamount != 'None' ? '<strong>player.betamount+1</strong>' : '<strong>1</strong>')
+        .replace('_maxbet_', `<strong>${balance_for_bets}</strong>`)
+    );
+
+    $('#modal-currentbet').hide();
     if (player.betexpdate != 'None') {
         $('#modal-currentbet').show();
         $('#modal-pl-bestbetteam').val(player.betteam);
