0e1507cd0a995833dcdeec54f99482b736c0415a
Giambattista Aresu <giambattista.aresu@gmail.com>
2025-08-16 19:15:11 +0200

    migration: table vote

---
+++ l4m20/l4m_app/single_models/vote.py
@@ -0,0 +1,30 @@
+from l4m20 import constants as C
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Vote(models.Model):
+
+    Competition = models.ForeignKey(on_delete=models.CASCADE,to=C.Constant_Strings.competition, null=True)
+    Player = models.ForeignKey(on_delete=models.CASCADE, to=C.Constant_Strings.player, null=True)
+    Day = models.IntegerField()              # day of play
+    Vote = models.FloatField(default=0.0)    # vote
+    GoalSc = models.IntegerField(default=0)  # gol scored
+    GoalTa = models.IntegerField(default=0)  # gol taken
+    GoalDe = models.IntegerField(default=0)  # bonus: decider, gol partita, not used, maybe one day, maybe not
+    PenSc = models.IntegerField(default=0)   # penalty scored
+    PenMi = models.IntegerField(default=0)   # penalty missed
+    PenSa = models.IntegerField(default=0)   # penalty saved
+    Own = models.IntegerField(default=0)     # own goal
+    Yel = models.IntegerField(default=0)     # yellow card
+    Red = models.IntegerField(default=0)     # red card
+    AssS = models.IntegerField(default=0)    # assist standard
+    AssH = models.IntegerField(default=0)    # assist high # not necessarily to be used
+    AssL = models.IntegerField(default=0)    # assist low # not necessarily to be used
+    AssP = models.IntegerField(default=0)    # penalty procured
+    SubJ = models.IntegerField(default=0)    # sub-judice: all bonuses are to be confirmed
+    Sub = models.FloatField(default=0.0)     # substitution: +30 in at minute 30, -87 out at minute 87
+    
+
+    def __str__(self):
+        return f"{self.player} - Giornata {self.day} ({self.vote})"
