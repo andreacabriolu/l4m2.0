ec65ecf78483a451048ed4663a430b59a6c1913c
Giambattista Aresu <giambattista.aresu@gmail.com>
2025-08-17 15:08:17 +0200

    adding comp_calendar,matches_calendar,matches_results

--- l4m20/l4m_app/models.py
+++ l4m20/l4m_app/models.py
@@ -1,4 +1,4 @@
 from django.db import models
 
-from .single_models import session, bet, competition, market, player, real_team, series, team, balance, notification, bet_history, squads, lineup, real_calendar, mark_players, vote
+from .single_models import session, bet, competition, market, player, real_team, series, team, balance, notification, bet_history, squads, lineup, real_calendar, mark_players, vote, competition_calendar, matches_calendar, matches_results
 
