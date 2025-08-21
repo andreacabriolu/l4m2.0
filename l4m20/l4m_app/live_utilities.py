b24d3f080036370079ac50a70352f50a72e826c1
Andrea Cabriolu <andrea.cabriolu@inaf.it>
2025-08-21 02:56:35 +0200

    deleted locale settings

--- l4m20/l4m_app/live_utilities.py
+++ l4m20/l4m_app/live_utilities.py
@@ -34,8 +34,8 @@ def make_empty_vote_obj(pl_id, cap_id, already_played, current_day):
     v_obj.Status = C.PlayerStatus.YET_TO_PLAY if not already_played else C.PlayerStatus.NOT_PLAYED
     real_match = real_calendar.Real_calendar.objects.filter(Q(Day=current_day) & \
                                                             (Q(RealTeamHome_id=pl.RealTeam) | Q(RealTeamAway_id=pl.RealTeam)))
-    locale.setlocale(locale.LC_ALL, 'it_IT')
-    v_obj.Msg = real_match[0].Date.strftime('%d %B %Y alle %H:%M') if real_match else ""
+    # locale.setlocale(locale.LC_ALL, 'it_IT')
+    v_obj.Msg = real_match[0].Date.strftime('%d-%m-%Y alle %H:%M') if real_match else ""
 
     return v_obj
 
