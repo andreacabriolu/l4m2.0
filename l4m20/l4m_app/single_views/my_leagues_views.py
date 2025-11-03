7431c4a91d16c1bbc92d449ec0a584e5c521b6d6
Andrea Cabriolu <andreacabriolu@gmail.com>
2025-11-03 03:14:50 +0100

    starting my leagues pages

---
+++ l4m20/l4m_app/single_views/my_leagues_views.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+import json
+from django.db.models import Q
+
+from .. import utilities as U
+from ..models import *
+from l4m20 import constants as C
+
