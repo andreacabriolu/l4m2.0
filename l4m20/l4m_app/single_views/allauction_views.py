4f9e1028e37676176e1919d0ae85c1675c90c10b
Andrea Cabriolu <andreacabriolu@gmail.com>
2026-08-11 11:39:21 +0200

    changed order for all auctions

--- l4m20/l4m_app/single_views/allauction_views.py
+++ l4m20/l4m_app/single_views/allauction_views.py
@@ -65,10 +65,10 @@ class AllAuctionsView(LoginRequiredMixin, View):
             if len(balances_) <= 0: 
                 continue
 
-            lp.sort(key=lambda x: x['bet__Expiration_Date'], reverse=True)
-            ld.sort(key=lambda x: x['bet__Expiration_Date'], reverse=True)
-            lc.sort(key=lambda x: x['bet__Expiration_Date'], reverse=True)
-            la.sort(key=lambda x: x['bet__Expiration_Date'], reverse=True)
+            lp.sort(key=lambda x: x['bet__Expiration_Date'])
+            ld.sort(key=lambda x: x['bet__Expiration_Date'])
+            lc.sort(key=lambda x: x['bet__Expiration_Date'])
+            la.sort(key=lambda x: x['bet__Expiration_Date'])
 
             balance = balances_[0]
             balance_for_bets = U.get_balance_for_bets(_team.id, balance['Purchases_max'])
